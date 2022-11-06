a20aa4465f2c2329f9fffec0a10e2fd64bc49ff0
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-05 22:28:18 -0300

    feat/ example

---
+++ examples/example.js
@@ -0,0 +1,21 @@
+import euler from '../eulejs.js'
+
+const diagram = euler(
+    {
+        'a': [1, 2, 3],
+        'b': [2, 3, 4],
+        'c': [3, 4, 5],
+        'd': [3, 5, 6]
+    })
+
+/* Euler dictionary: 
+ *	 {
+ *		'a,b': [2], 
+ * 		'b,c': [4], 
+ * 		'a,b,c,d': [3], 
+ *		'c,d': [5], 
+ *		'd': [6], 
+ *		'a': [1]
+ *	 }
+ */
+console.log(diagram)
